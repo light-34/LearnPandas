cc38d915757765ab6ef4d499d3ce0f6e0de419bc
cezmi.aktepe <cezmi.aktepe@vtinfo.com>
2025-08-14 13:50:09 -0400

    data frame functions added

--- src/data_frame/data_frame.py
+++ src/data_frame/data_frame.py
@@ -15,4 +15,50 @@ tr8 = pd.DataFrame({
 
 tr9 = pd.merge(tr7, tr8, on='idx', how='inner') #how= inner, left or right
 
-print(f'TR9 :\n {tr9}')
+#print(f'TR9 :\n {tr9}')
+
+#************ AGG ***********
+agg = np.arange(20).reshape(4,5)
+aggPd = pd.DataFrame(agg)
+# print(aggPd)
+
+# print(aggPd.agg(['sum', 'min', 'max']))
+
+
+#************ TRANSFORM ***********
+tr = pd.DataFrame({"A": range(5), "B": range(10, 15)})
+
+def add_five(x):
+    return x + 5
+
+tr1 = tr.transform(lambda x: x ** 2)
+
+tr2 = tr.transform(add_five)
+
+# print(tr2)
+
+
+#************ GROUPBY ***********
+gb = pd.DataFrame({'Animal': ['Falcon', 'Falcon',
+                              'Parrot', 'Parrot'],
+                   'Max Speed': [380., 370., 24., 26.]})
+gbg = gb.groupby('Animal').mean()
+# print(gbg)
+
+gbArr = [['Falcon', 'Falcon', 'Parrot', 'Parrot'],
+          ['Captive', 'Wild', 'Captive', 'Wild']]
+gbIdx = pd.MultiIndex.from_arrays(gbArr, names=('Animal', 'Type'))
+gb1 = pd.DataFrame({'Max Speed': [390., 350., 30., 20.]}, index=gbIdx)
+
+gbg1 = gb1.groupby(level=0).mean()
+gbg2 = gb1.groupby(level='Type').mean()
+# print(gb3)
+
+gb4 = [["a", 12, 12], [None, 12.3, 33.], ["b", 12.3, 123], ["a", 1, 1]]
+gb5 = pd.DataFrame(gb4, columns=["a", "b", "c"])
+gbg3 = gb5.groupby(by='a').sum()
+gbg4 = gb5.groupby(by='a', dropna=False).sum()
+print(gbg4)
+
+#https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.groupby.html
+
